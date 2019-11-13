391edc36ba5c7524bc3e7e38ff545e3bc43cf95c
taiki-nishi <beefupme@gmail.com>
2019-11-13 14:30:59 +0900

    add rest api

--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,10 @@
+const url = "https://bozuman.cybozu.com/k/v1/record.json?app=43250&id=307";
+
 export default () => {
   console.log("hello");
-}
+  fetch(url, {
+    headers: { "X-Cybozu-Authorization": "" }
+  })
+    .then(response => response.json())
+    .then(json => console.log(json));
+};
