e1b5c781f761911a515e6761e9d4fcaa937324c0
taiki-nishi <beefupme@gmail.com>
2019-11-13 14:10:41 +0900

    ディレクトリを移動

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
 module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: "./src/index.ts",
+  output: {
+    path: path.join(__dirname,"dist/js/"),
+    filename: "index.js",
+  },
   module: {
     rules: [
       {
