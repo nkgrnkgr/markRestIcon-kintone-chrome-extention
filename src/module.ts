2fcfd1f36d603ba861e4c17e2cc6675c98e89717
Nakata Yudai <yudai-nakata@cybozu.co.jp>
2019-11-13 15:03:46 +0900

    認証キーをenvファイルから取得

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,8 @@
-const url = "https://bozuman.cybozu.com/k/v1/record.json?app=43250&id=307";
+const url = "https://bozuman.cybozu.com/k/v1/records.json?app=43250&query=取得日%20in%20(TODAY())";
 
 export default () => {
-  console.log("hello");
   fetch(url, {
-    headers: { "X-Cybozu-Authorization": "" }
+    headers: { "X-Cybozu-Authorization": process.env.AUTH_KEY }
   })
     .then(response => response.json())
     .then(json => console.log(json));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+require('dotenv').config();
 module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: "./src/index.ts",
@@ -18,5 +20,10 @@ module.exports = {
     extensions: [
       ".ts"
     ]
-  }
-}
+  },
+  plugins : [
+    new webpack.DefinePlugin({
+      "process.env.AUTH_KEY": JSON.stringify(process.env.AUTH_KEY),
+    })
+  ],
+};
