24676a5751089a42e266b402225d28321e8fb965
taiki-nishi <beefupme@gmail.com>
2019-11-13 16:37:37 +0900

    非同期処理のあとにstyleを当てるのが難しそう

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-chrome.runtime.sendMessage("api", (response) => {
+chrome.runtime.sendMessage("api", response => {
   console.log(response);
 });
+
+document.getElementsByClassName("aa");
+const a: NodeListOf<HTMLLIElement> = document.querySelectorAll(
+  ".ocean-ui-plugin-mention-ac-item-icon"
+);
+
+a.forEach(item => {
+  item.style.opacity = "0";
+});
+// a.forEach(item => item.style.)
