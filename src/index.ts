2622b1c95b26bd761095f562f05a61c375eaf163
sakai <yasuharu-sakai@cybozu.co.jp>
2019-11-14 14:13:39 +0900

    WIP: idを取り出して、判定をし始めたところ

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import childListObserver from "./lib/childListObserver";
+const cache = [];
 
 const mutateIcon = () => {
   childListObserver(".ocean-ui-plugin-mention-ac-item-icon", icons => {
     for (const icon of icons) {
+      const parser = new URL(icon.getAttribute("src"));
+      console.log(parser.search.match(/id=([0-9]+)/)[1]); // TODO: 写真がないと死ぬ
       icon.style.opacity = "50%";
     }
   });
@@ -14,7 +17,10 @@ chrome.storage.local.get(["saved_time"], function(obj) {
   if (saved_time === undefined || Date.now() - Number(saved_time) > 5 * 1000) {
     // APIの実行
     chrome.runtime.sendMessage("api", response => {
-      console.log(response);
+      for (let index = 0; index < response.users.length; index++) {
+        const { id } = response.users[index];
+        cache.push(id);
+      }
       mutateIcon();
     });
   }
